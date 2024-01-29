79f36f11503dc1449566acb30f27fd427b71fb4b
Navith <142579014+NAVITH123@users.noreply.github.com>
2024-01-29 12:15:34 +0530

    Add files via upload

---
+++ review.js
@@ -0,0 +1,47 @@
+<script src='https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/textfit/2.4.0/textFit.min.js'></script>
+/**
+  Swiper:
+  https://swiperjs.com/
+**/
+const swiper = new Swiper(".swiperCarousel", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 10,
+    keyboard: {
+      enabled: true,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
+  const slides = document.getElementsByClassName("swiper-slide");
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      const { className } = slide;
+      if (className.includes("swiper-slide-next")) {
+        swiper.slideNext();
+      } else if (className.includes("swiper-slide-prev")) {
+        swiper.slidePrev();
+      }
+    });
+  }
+  
+  function resizeTextToFit() {
+    const quoteEls = document.getElementsByClassName('quote');
+    for (const el of quoteEls) {
+      el.style.width = el.offsetWidth;
+      el.style.height = el.offsetHeight;
+    }
+    textFit(quoteEls, { maxFontSize: 14 });
+  }
+  resizeTextToFit();
+  addEventListener("resize", (event) => {
+    resizeTextToFit();
+  });
